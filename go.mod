4dca73cc3a76168f123fc4532e13c41324a20d28
RayChang <ray.chang@silkrode.com.tw>
2020-07-08 22:07:31 +0800

    add database conn

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/karta0898098/kara
 
 go 1.14
 
-require github.com/jinzhu/gorm v1.9.14 // indirect
+require (
+	github.com/cenkalti/backoff/v4 v4.0.2
+	github.com/jinzhu/gorm v1.9.14
+	github.com/pkg/errors v0.9.1
+)
--- pkg/db/config.go
+++ pkg/db/config.go
@@ -1,6 +1,15 @@
 package db
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"github.com/cenkalti/backoff/v4"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/pkg/errors"
+	"strconv"
+	"time"
+)
 
 type DatabaseType string
 
@@ -15,17 +24,18 @@ type Config struct {
 	Read    Database
 	Write   Database
 	Secrets string
-	Color   bool
 }
 
 type Database struct {
+	Debug          bool
 	Host           string
 	User           string
 	Port           int
 	Password       string
-	Database       string
+	Name           string
 	Type           DatabaseType
-	MaxIdleConn    int
+	MaxIdleConns   int
+	MaxOpenConns   int
 	MaxLifetimeSec int
 	ReadTimeout    string
 	WriteTimeout   string
@@ -34,4 +44,89 @@ type Database struct {
 type Connection struct {
 	ReadDB  *gorm.DB
 	WriteDB *gorm.DB
+}
+
+func NewConnection(cfg Config) (*Connection, error) {
+
+	readDB, err := setupDatabase(&cfg.Read)
+	if err != nil {
+		return nil, err
+	}
+	writeDB, err := setupDatabase(&cfg.Write)
+	if err != nil {
+		return nil, err
+	}
+	readDB.LogMode(cfg.Read.Debug)
+	writeDB.LogMode(cfg.Write.Debug)
+
+	return &Connection{
+		ReadDB:  readDB,
+		WriteDB: writeDB,
+	}, nil
+}
+
+func setupDatabase(database *Database) (*gorm.DB, error) {
+	bo := backoff.NewExponentialBackOff()
+	bo.MaxElapsedTime = time.Duration(180) * time.Second
+
+	if database.WriteTimeout == "" {
+		database.WriteTimeout = "10s"
+	}
+
+	if database.ReadTimeout == "" {
+		database.ReadTimeout = "10s"
+	}
+
+	driver := ""
+	dsn := ""
+	switch database.Type {
+	case MySQL:
+		driver = string(MySQL)
+		dsn = fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true&multiStatements=true&readTimeout=%s&writeTimeout=%s", database.User, database.Password, database.Host+":"+strconv.Itoa(database.Port), database.Name, database.ReadTimeout, database.WriteTimeout)
+	case Postgres:
+		driver = string(Postgres)
+		dsn = fmt.Sprintf(`user=%s password=%s host=%s port=%d dbname=%s sslmode=disable `, database.User, database.Password, database.Host, database.Port, database.Name)
+	default:
+		return nil, errors.New("Not support driver")
+	}
+
+	var conn *gorm.DB
+
+	// 嘗試重新連線database
+	err := backoff.Retry(func() error {
+		db, err := gorm.Open(driver, dsn)
+		if err != nil {
+			return err
+		}
+		err = db.DB().Ping()
+		if err != nil {
+			return err
+		}
+
+		conn = db
+		return nil
+	}, bo)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// set default idle conn
+	if database.MaxIdleConns == 0 {
+		database.MaxIdleConns = 10
+	}
+
+	if database.MaxOpenConns == 0 {
+		database.MaxOpenConns = 20
+	}
+
+	if database.MaxLifetimeSec == 0 {
+		database.MaxLifetimeSec = 14400
+	}
+
+	conn.DB().SetMaxIdleConns(database.MaxIdleConns)
+	conn.DB().SetMaxOpenConns(database.MaxOpenConns)
+	conn.DB().SetMaxIdleConns(database.MaxLifetimeSec)
+
+	return conn, nil
 }
