079316bfa59a440187a58fb6861b1957ab2d760c
RayChang <ray.chang@silkrode.com.tw>
2020-08-03 11:01:25 +0800

    add condition

---
+++ db/condition/sorting.go
@@ -0,0 +1,19 @@
+package condition
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
+
+type Sorting struct {
+	SortField string
+	SortOrder string
+}
+
+func (s *Sorting) Sort(db *gorm.DB) *gorm.DB {
+	if len(s.SortField) != 0 && len(s.SortOrder) != 0 {
+		db = db.Order(fmt.Sprintf("%s %s", s.SortField, s.SortOrder))
+	}
+
+	return db
+}
