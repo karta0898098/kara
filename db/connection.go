d37ca1ebc4269094d0fbf74d37fcc0d0feab7ba1
RayChang <ray.chang@silkrode.com.tw>
2020-09-01 15:54:43 +0800

    add gorm logger

--- db/connection.go
+++ db/connection.go
@@ -4,12 +4,11 @@ import (
 	"fmt"
 	"github.com/cenkalti/backoff/v4"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	"log"
-	"os"
 	"strconv"
 	"time"
 )
@@ -43,72 +42,6 @@ type Database struct {
 	WriteTimeout   string       `mapstructure:"write_timeout"`
 }
 
-//func SetupDatabase(database *Database) (*gorm.DB, error) {
-//	bo := backoff.NewExponentialBackOff()
-//	bo.MaxElapsedTime = time.Duration(180) * time.Second
-//
-//	if database.WriteTimeout == "" {
-//		database.WriteTimeout = "10s"
-//	}
-//
-//	if database.ReadTimeout == "" {
-//		database.ReadTimeout = "10s"
-//	}
-//
-//	driver := ""
-//	dsn := ""
-//	switch database.Type {
-//	case MySQL:
-//		driver = string(MySQL)
-//		dsn = fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true&multiStatements=true&readTimeout=%s&writeTimeout=%s", database.User, database.Password, database.Host+":"+strconv.Itoa(database.Port), database.Name, database.ReadTimeout, database.WriteTimeout)
-//	case Postgres:
-//		driver = string(Postgres)
-//		dsn = fmt.Sprintf(`user=%s password=%s host=%s port=%d dbname=%s sslmode=disable `, database.User, database.Password, database.Host, database.Port, database.Name)
-//	default:
-//		return nil, errors.New("Not support driver")
-//	}
-//
-//	var conn *gorm.DB
-//
-//	// 嘗試重新連線database
-//	err := backoff.Retry(func() error {
-//		db, err := gorm.Open(driver, dsn)
-//		if err != nil {
-//			return err
-//		}
-//		err = db.DB().Ping()
-//		if err != nil {
-//			return err
-//		}
-//
-//		conn = db
-//		return nil
-//	}, bo)
-//
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	// set default idle conn
-//	if database.MaxIdleConns == 0 {
-//		database.MaxIdleConns = 10
-//	}
-//
-//	if database.MaxOpenConns == 0 {
-//		database.MaxOpenConns = 20
-//	}
-//
-//	if database.MaxLifetimeSec == 0 {
-//		database.MaxLifetimeSec = 14400
-//	}
-//
-//	conn.DB().SetMaxIdleConns(database.MaxIdleConns)
-//	conn.DB().SetMaxOpenConns(database.MaxOpenConns)
-//	conn.DB().SetMaxIdleConns(database.MaxLifetimeSec)
-//
-//	return conn, nil
-//}
-
 func SetupDatabase(database *Database) (*gorm.DB, error) {
 	bo := backoff.NewExponentialBackOff()
 	bo.MaxElapsedTime = time.Duration(180) * time.Second
@@ -141,7 +74,11 @@ func SetupDatabase(database *Database) (*gorm.DB, error) {
 		logLevel = logger.Info
 	}
 	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		//log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		&GormLogger{
+			logger: log.With().Logger(),
+			WithColor: colorful,
+		},
 		logger.Config{
 			SlowThreshold: time.Second, // Slow SQL threshold
 			LogLevel:      logLevel,    // Log level
