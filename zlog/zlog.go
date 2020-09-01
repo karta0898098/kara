e4dae1db44d3d3559d22f49e3ffc369419e05d7b
RayChang <ray.chang@silkrode.com.tw>
2020-09-01 16:36:25 +0800

    update logger

--- zlog/zlog.go
+++ zlog/zlog.go
@@ -1,7 +1,6 @@
 package zlog
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/rs/zerolog"
@@ -83,16 +82,16 @@ func Setup(config *Config) {
 			Out: os.Stdout,
 		}
 		output.FormatMessage = func(i interface{}) string {
-			return fmt.Sprintf("\n[\n%s\n]\n", i)
+			return fmt.Sprintf("\n[\n\t%s\n]\n", i)
 		}
 		output.FormatFieldName = func(i interface{}) string {
 			return fmt.Sprintf("%s:", Teal(i))
 		}
 		output.FormatFieldValue = func(i interface{}) string {
-			return fmt.Sprintf("%s", i)
+			return fmt.Sprintf("%s \n", i)
 		}
 		output.FormatTimestamp = func(i interface{}) string {
-			t := fmt.Sprintf("%s", i)
+			t := fmt.Sprintf("%v", i)
 			millisecond, err := strconv.ParseInt(fmt.Sprintf("%s", i), 10, 64)
 			if err == nil {
 				t = time.Unix(int64(millisecond/1000), 0).Local().Format("2006/01/02 15:04:05")
@@ -115,13 +114,3 @@ func Setup(config *Config) {
 		Level(level)
 }
 
-// Ctx wrap zerolog Ctx func, if ctx not setting Logger, return a default prevent for panic
-func Ctx(ctx context.Context) *zerolog.Logger {
-	defalutLogger := log.Logger
-	if ctx == nil {
-		defalutLogger.Warn().Msg("zlog func Ctx() not set context.Context in right way.")
-		return &defalutLogger
-	}
-
-	return log.Ctx(ctx) // if ctx is not null and not set Logger yet. A disabled Logger is returned.
-}
